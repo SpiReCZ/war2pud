cf6b30817c9ca2ccc8d5ad243e842d861729e379
SpiReCZ <SpiReCZ@users.noreply.github.com>
2022-12-10 23:57:05 +0100

    reformat source code

--- war2pud/exception.py
+++ war2pud/exception.py
@@ -8,19 +8,24 @@
 #  Author: Beau Hastings <beausy@gmail.com>
 
 class PudFileError(Exception):
-  """ Raised when PUD file format errors are encountered. """
+    """ Raised when PUD file format errors are encountered. """
+
 
 class SectionError(Exception):
-  """ Raised when a section name is not recognized. """
+    """ Raised when a section name is not recognized. """
+
 
 class VersionError(Exception):
-  """ Raised when the PUD file version is not recognized. """
+    """ Raised when the PUD file version is not recognized. """
+
 
 class PlayerError(Exception):
-  """ Raised when an unknown player type is encountered. """
+    """ Raised when an unknown player type is encountered. """
+
 
 class MapError(Exception):
-  """ Raised when map dimensions are out of bounds. """
+    """ Raised when map dimensions are out of bounds. """
+
 
 class TerrainError(Exception):
-  """ Raised when an unknown terrain type is encountered. """
+    """ Raised when an unknown terrain type is encountered. """
